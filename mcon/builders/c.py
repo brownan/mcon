aad89564e0adce676b0ad72ebbacfb98beac5da7
Andrew Brown <andrew@brownan.org>
2023-03-04 17:14:36 -0500

    Added readme

--- mcon/builders/c.py
+++ mcon/builders/c.py
@@ -1,7 +1,7 @@
 import dataclasses
 import shlex
 import subprocess
-from typing import Collection, Sequence
+from typing import Collection, List, Optional, Sequence
 
 from mcon import Environment, File, FileLike, FileSetLike
 from mcon.builder import SingleFileBuilder
@@ -35,17 +35,21 @@ class CompilerConfig:
     lib_dirs: Collection[str] = ()
 
 
+def get_default_compiler_config() -> CompilerConfig:
+    return CompilerConfig()
+
+
 class CompiledObject(SingleFileBuilder):
     def __init__(
         self,
         env: Environment,
         target: FileLike,
         sources: FileSetLike,
-        compiler_config: CompilerConfig,
+        compiler_config: Optional[CompilerConfig] = None,
     ):
         super().__init__(env, target)
         self.sources = self.depends_files(sources)
-        self.compiler_config = compiler_config
+        self.compiler_config = compiler_config or get_default_compiler_config()
 
     def __str__(self) -> str:
         return f"Compiling {self.target}"
@@ -66,17 +70,17 @@ class CompiledObject(SingleFileBuilder):
         subprocess.check_call(cmdline)
 
 
-class SharedLibrary(SingleFileBuilder):
+class Program(SingleFileBuilder):
     def __init__(
         self,
         env: Environment,
-        target: File,
+        target: FileLike,
         sources: FileSetLike,
-        compiler_config: CompilerConfig,
+        compiler_config: Optional[CompilerConfig] = None,
     ):
         super().__init__(env, target)
         self.sources = self.depends_files(sources)
-        self.compiler_config = compiler_config
+        self.compiler_config = compiler_config or get_default_compiler_config()
 
     def __str__(self) -> str:
         return f"Linking {self.target}"
@@ -84,9 +88,9 @@ class SharedLibrary(SingleFileBuilder):
     def get_targets(self) -> File:
         return self.target
 
-    def build(self) -> None:
+    def get_cmdline(self) -> List[str]:
         conf = self.compiler_config
-        cmdline = shlex.split(conf.cc)
+        cmdline = shlex.split(conf.ld)
         cmdline += [
             "-o",
             str(self.target.path),
@@ -95,4 +99,16 @@ class SharedLibrary(SingleFileBuilder):
         for libdir in conf.lib_dirs:
             cmdline.extend(["-L", libdir])
         cmdline.extend(str(s.path) for s in self.sources)
+        return cmdline
+
+    def build(self) -> None:
+        cmdline = self.get_cmdline()
         subprocess.check_call(cmdline)
+
+
+class SharedLibrary(Program):
+    def get_cmdline(self) -> List[str]:
+        cmdline = super().get_cmdline()
+        if "-shared" not in cmdline:
+            cmdline.append("-shared")
+        return cmdline
